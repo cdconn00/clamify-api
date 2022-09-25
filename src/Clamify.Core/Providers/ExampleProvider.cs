dc4da801a456f48eed0f335cbd3763616e973e89
Cole D. Connelly <cdconn00@gmail.com>
2022-09-25 14:39:53 -0500

    Add example provider, unit test example.

---
+++ src/Clamify.Core/Providers/ExampleProvider.cs
@@ -0,0 +1,28 @@
+﻿using Clamify.Core.Providers.Interfaces;
+using Clamify.Entities;
+using Clamify.Entities.Context;
+
+namespace Clamify.Core.Providers;
+
+/// <summary>
+/// Implementation of provider to get examples from the DB.
+/// </summary>
+public class ExampleProvider : IExampleProvider
+{
+    private readonly ClamifyContext _context;
+
+    /// <summary>
+    /// Constructs the provider
+    /// </summary>
+    /// <param name="context">The clamify database context to pull from.</param>
+    public ExampleProvider(ClamifyContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<Example> Get()
+    {
+        return _context.Examples;
+    }
+}
---
+++ src/Clamify.Core/Providers/Interfaces/IExampleProvider.cs
@@ -0,0 +1,15 @@
+﻿using Clamify.Entities;
+
+namespace Clamify.Core.Providers.Interfaces;
+
+/// <summary>
+/// Contract defining methods to retrieve examples.
+/// </summary>
+public interface IExampleProvider
+{
+    /// <summary>
+    /// Gets a list of examples from the database context.
+    /// </summary>
+    /// <returns>A list of example from the DB (if any).</returns>
+    public IEnumerable<Example> Get();
+}
