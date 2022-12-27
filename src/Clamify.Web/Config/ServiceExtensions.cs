72c49d442786bd00904511829ed4e3f38a3778b3
Cole D. Connelly <cdconn00@gmail.com>
2022-12-27 11:45:36 -0600

    Add extension to get secrets.

--- src/Clamify.Web/Config/ServiceExtensions.cs
+++ src/Clamify.Web/Config/ServiceExtensions.cs
@@ -1,6 +1,10 @@
-﻿using Clamify.Entities;
+﻿using System.Text;
+using Clamify.Entities;
 using Clamify.Entities.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Clamify.Web.Config;
 
@@ -17,5 +21,23 @@ public static class ServiceExtensions
     {
         IdentityBuilder builder = services.AddIdentity<ClamifyUser, IdentityRole<int>>(u => u.User.RequireUniqueEmail = true);
         builder.AddEntityFrameworkStores<ClamifyContext>().AddDefaultTokenProviders();
+    }
+
+    /// <summary>
+    /// Retrieves a secret from the local development or server environment depending on where the application is running.
+    /// </summary>
+    /// <param name="webApplicationBuilder">The builder used to determine the environment.</param>
+    /// <param name="secretName">The name of the secret to retrieve.</param>
+    /// <returns>The value of the secret requested.</returns>
+    public static string GetSecret(WebApplicationBuilder webApplicationBuilder, string secretName)
+    {
+        if (webApplicationBuilder.Environment.IsDevelopment())
+        {
+            return webApplicationBuilder.Configuration[secretName] ?? throw new InvalidOperationException($"{secretName} not found.");
+        }
+        else
+        {
+            return Environment.GetEnvironmentVariable(secretName) ?? throw new InvalidOperationException($"{secretName} not found.");
+        }
     }
 }
