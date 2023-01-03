07b85c15e506a2c3bd3aedeef6c8217bb9f46686
Cole D. Connelly <cdconn00@gmail.com>
2023-01-03 13:29:24 -0600

    Scaffold user controller / create endpoint.

---
+++ src/Clamify.Web/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using Clamify.Core.Managers.Interfaces;
+using Clamify.Core.Providers.Interfaces;
+using Clamify.Core.Writers.Interfaces;
+using Clamify.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clamify.Web.Controllers;
+
+/// <summary>
+/// Contains endpoints to interact with the user object.
+/// </summary>
+public class UsersController : Controller
+{
+    private readonly UserManager<ClamifyUser> _userManager;
+    private readonly ILogger<UsersController> _logger;
+    private readonly IFeatureFlagProvider _featureFlagProvider;
+    private readonly IMessageWriter messageWriter;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UsersController"/> class.
+    /// </summary>
+    /// <param name="userManager">Provides access to user objects.</param>
+    /// <param name="logger">Provides methods for logging errors.</param>
+    /// <param name="featureFlagProvider">Provides methods for determining if a feature is enabled.</param>
+    /// <param name="messageWriter">Allows an email to be sent to a user.</param>
+    public UsersController(
+        UserManager<ClamifyUser> userManager,
+        ILogger<UsersController> logger,
+        IFeatureFlagProvider featureFlagProvider,
+        IMessageWriter messageWriter)
+    {
+        _userManager = userManager;
+        _logger = logger;
+        _featureFlagProvider = featureFlagProvider;
+        this.messageWriter = messageWriter;
+    }
+
+    /// <summary>
+    /// Registers a new user for the service and sends them a email verification code.
+    /// </summary>
+    /// <returns>The result of the creation request.</returns>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> Create()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/Clamify.Tests/Web/Controllers/UsersControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Clamify.Core.Providers.Interfaces;
+using Clamify.Core.Writers.Interfaces;
+using Clamify.Entities;
+using Clamify.Tests.Mocks;
+using Clamify.Web.Controllers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Clamify.Tests.Web.Controllers;
+
+/// <summary>
+/// Unit test class verifies <see cref="UsersController"/> endpoints.
+/// </summary>
+[TestClass]
+public class UsersControllerTests
+{
+    private Mock<UserManager<ClamifyUser>> _userManager;
+    private ILogger<UsersController> _logger;
+    private IFeatureFlagProvider _featureFlagProvider;
+    private IMessageWriter _messageWriter;
+
+    private UsersController GetController =>
+        new UsersController(_userManager.Object, _logger, _featureFlagProvider, _messageWriter);
+
+    /// <summary>
+    /// Initialize the tests with a mocks.
+    /// </summary>
+    [TestInitialize]
+    public void Initialize()
+    {
+        _userManager = IdentityMockHelpers.MockUserManager<ClamifyUser>();
+        _logger = Mock.Of<ILogger<UsersController>>();
+        _featureFlagProvider = Mock.Of<IFeatureFlagProvider>();
+        _messageWriter = Mock.Of<IMessageWriter>();
+    }
+}
